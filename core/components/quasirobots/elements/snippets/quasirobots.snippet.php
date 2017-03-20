9c64417aecc845600e233d4bcb887ef1492067f1
misha8000 <quasi-art@ya.ru>
2017-03-20 18:39:50 +0300

    Выводить адрес карты сайта по умолчанию.

--- core/components/quasirobots/elements/snippets/quasirobots.snippet.php
+++ core/components/quasirobots/elements/snippets/quasirobots.snippet.php
@@ -2,7 +2,7 @@
 $output = '';
 $site_status = (int)$modx->getOption('site_status', $scriptProperties, 1);
 $site_url = $modx->getOption('site_url', null, '');
-$sitemap = (int)$modx->getOption('sitemap', $scriptProperties, 0);
+$sitemap = $modx->getOption('sitemap', $scriptProperties, false);
 
 $disallow = [
 	'User-agent: *',
@@ -21,8 +21,10 @@ $offlineText = [
 if ($site_status === 1) {
 	$output = implode("\n", $disallow);
 	$output .= "\nHost: ".$site_url;
-	if ($sitemap > 0) {
+	if (intval($sitemap) > 0) {
 		$output .= "\nSitemap: ".$modx->makeUrl($sitemap, '', '', 'full');
+	} elseif (!$sitemap) {
+		$output .= "\nSitemap: ".$site_url.'sitemap.xml';
 	}
 } else {
 	$output = implode("\n", $offlineText);
